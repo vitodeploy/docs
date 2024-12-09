8f9f4bbc50e8b5238c896ac88eb5cb3884005829
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2024-12-09 23:02:48 +0100

    fix color

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -7,7 +7,7 @@ const {Layout} = DefaultTheme
 <template>
   <Layout>
     <template #layout-top>
-      <div style="height: 30px; display: flex; align-items: center; justify-content: center; background: #4E44E6; width: 100%; text-align: center; position: fixed; top: 0; left: 0; right: 0; z-index: 100;">
+      <div style="height: 30px; display: flex; align-items: center; justify-content: center; background: #4E44E6; width: 100%; text-align: center; position: fixed; top: 0; left: 0; right: 0; z-index: 100; color: white !important;">
         VitoDeploy v2 is out! 🎉 <a href="https://vitodeploy.com" style="text-decoration: underline;">Check Here</a>
       </div>
     </template>
